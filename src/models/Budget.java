7c962cda6196f66c79ef3a20ad845155f3b1c9cf
DubbSwitch <72952746+DubbSwitch@users.noreply.github.com>
2021-03-06 15:50:09 -0700

    * filled out budget class

--- src/models/Budget.java
+++ src/models/Budget.java
@@ -1,5 +1,54 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Budget {
 
+    private ArrayList<Double> budgetOverTime = new ArrayList<>();
+    private double funds;
+    private String name;
+
+    public double deposit(double amount) {
+
+        return 0;
+    }
+
+    public double withdraw(double amount) {
+
+        return 0;
+    }
+
+    public ArrayList<Double> getBudgetOverTime() {
+        return budgetOverTime;
+    }
+
+    public void setBudgetOverTime(ArrayList<Double> budgetOverTime) {
+        this.budgetOverTime = budgetOverTime;
+    }
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Budget(double funds, String name) {
+        setFunds(funds);
+        setName(name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
