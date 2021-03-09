42c548672e0e9649b4a618d9e04c2d73618afb27
DubbSwitch <72952746+DubbSwitch@users.noreply.github.com>
2021-03-08 23:53:14 -0700

    Add files via upload

---
+++ src/prog/Main.java
@@ -0,0 +1,10 @@
+package prog;
+
+import controllers.MCBudgetController;
+
+public class Main {
+
+    public static void main(String[] args) { run(); }
+
+    public static void run() { MCBudgetController.run(); }
+}
