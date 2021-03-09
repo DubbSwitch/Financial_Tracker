084f3083a37c6f58ce1d3e02a00a2e6ebba05eda
isaac langi <ilangi@student.neumont.edu>
2021-03-08 20:07:49 -0700

    Added two methods to the FileIO.java
    Signed-off-by: isaac langi <ilangi@student.neumont.edu>

--- src/views/FileIO.java
+++ src/views/FileIO.java
@@ -1,5 +1,32 @@
 package views;
 
-public class FileIO {
+import java.io.*;
 
+public class FileIO {
+    public static void writeTextToFile(String filePath, String text){
+        try {
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.write(text);
+            myWriter.close();
+        }catch (IOException ioe) {
+            System.out.println("error");
+            ioe.printStackTrace();
+        }
+    }
+    public static String readTextFromFile(String filePath) {
+        File something = new File(filePath);
+        String text = "Couldn't find anything";
+        if(something.exists()) {
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(filePath));
+                text = br.readLine();
+
+            } catch (Exception fnfe) {
+                System.out.println("Error, diary entry doesn't exist");
+                fnfe.printStackTrace();
+            }
+        }
+        return text;
+    }
 }
