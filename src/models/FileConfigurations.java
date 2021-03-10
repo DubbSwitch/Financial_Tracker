4d42249b12bbbb6352e5dfb412690096cbcc443f
DubbSwitch <72952746+DubbSwitch@users.noreply.github.com>
2021-03-09 20:08:43 -0700

    * 09MAR2021-1834

--- src/models/FileConfigurations.java
+++ src/models/FileConfigurations.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 //TODO: IF YOU MODIFY THIS CLASS IN ANY WAY, YOU MUST UPDATE serialVersionUID TO A VALID SERIAL VERSION ACROSS ALL SERIALIZABLE CLASSES!!
 
 public class FileConfigurations implements Serializable {
-    private static final long serialVersionUID = 5L;
+    private static final long serialVersionUID = 4L;
     private Path path; // Path to dir where data is stored.
 
     public void setPath(Path path) {
